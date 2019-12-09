4bc7eb3fbd86e059cb67106d4a7d51334526b090
Abdullah Chaudhry <abdullahchaudhry0@outlook.com>
2019-12-10 01:48:59 +0500

    Run.py

--- run.py
+++ run.py
@@ -4,5 +4,18 @@ This python script will run the whole project and handle all the processes
 
 # imports
 from python_modules import read_socket
+import matplotlib.animation as animation
 
-read_socket.read_data(ip="192.168.137.195", delay=0.01)
+fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(8,4))
+fig.canvas.set_window_title('Jarrar tatti')
+
+ax[0][0].legend(['Ax'])
+ax[0][1].legend(['Ay'])
+ax[0][2].legend(['Az'])
+
+ax[1][0].legend(['Gx'])
+ax[1][1].legend(['Gy'])
+ax[1][2].legend(['Gz'])
+
+# read_socket.read_data(ip="192.168.137.195", delay=0.01)
+ani = animation.FuncAnimation(fig, read_socket.read_data, interval=1000, fargs=("192.168.137.195", 0.01, ax))
